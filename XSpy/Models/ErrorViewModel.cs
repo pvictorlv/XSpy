73441325c41f2413f67dddea716f80950fe4910c
Paulo Victor <b3t4t3st3@gmail.com>
2021-03-15 22:25:42 -0300

    Initial commit

---
+++ XSpy/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace XSpy.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
